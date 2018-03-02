8a6113f6dea570cdb96225f093f3c4ef00c9cae2
Dylan Oldfield <dylan.oldfield@yahoo.com>
2018-03-02 11:36:21 +1100

    Hello world

---
+++ LearningCplus/LearningCplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningCplus
+//
+//  Created by Dylan Oldfield on 2/3/18.
+//  Copyright © 2018 Dylan Oldfield. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
